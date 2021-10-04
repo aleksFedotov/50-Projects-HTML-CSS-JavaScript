1c504367aa5bf909413f2ee47f01a21011646a8f
aleksFedotov <aleksfedotov.dev@gmail.com>
2021-10-04 05:52:24 +0300

    git commit -m

---
+++ double-heart-click/script.js
@@ -0,0 +1,44 @@
+const loveMeEl = document.querySelector(".loveMe")
+const times = document.querySelector("#times")
+
+let clickTime = 0
+let timesClicked = 0
+
+loveMeEl.addEventListener("click", (e) => {
+    if (clickTime === 0) {
+        clickTime = new Date().getTime()
+
+    } else {
+        if ((new Date().getTime() - clickTime) < 800) {
+            createHeart(e)
+            clickTime = 0
+        } else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+function createHeart(e) {
+    const heart = document.createElement("i")
+    heart.classList.add("fas")
+    heart.classList.add("fa-heart")
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMeEl.appendChild(heart)
+
+    setTimeout(() => heart.remove(), 700)
+
+    times.innerHTML = ++timesClicked
+}
